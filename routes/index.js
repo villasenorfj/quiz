1caab0db583e32f0b52e2a0b501da2e5a2710b4c
villasenorfj <villasenorfj@gmail.com>
2015-08-18 20:22:45 +0200

    Quiz - Autenticacion de usuarios
    Paso 16 del proyecto QUIZ

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +14,12 @@ router.get('/', function(req, res, next) {
 router.get('/author', function(req, res, next) {
   res.render('author', { errors: [] });
 });
+
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
