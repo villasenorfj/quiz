a7387aec9b29bc94aa248950c7d74814cde05b23
villasenorfj <villasenorfj@gmail.com>
2015-08-18 19:55:33 +0200

    Quiz - Crear comentario
    Paso 15 del proyecto Quiz

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,8 +6,14 @@ exports.load = function(req, res, next, quizId) {
   .then(
     function(quiz) {
       if (quiz) {
-        req.quiz = quiz;
-        next();
+        models.Quiz.find({
+            where: { id: Number(quizId) },
+            include: [{ model: models.Comment }]
+        })
+        .then(function(quiz) {
+          req.quiz = quiz;
+          next();
+        })
       }else {
          next(new Error('No existe quizId=' + quizId))
       }
--- models/models.js
+++ models/models.js
@@ -28,8 +28,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+// Importar definicion de la tabla Comment
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+// Relaciones entre tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar definición de tabla Comment
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 // Adaptacion a promises por la version usada de sequelize
